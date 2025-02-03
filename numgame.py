8c32f36be053a8aa0f540e2b276cb4ad6e184dd2
Farhan-Akter-Arnab <162998arnab101m04ev@gmail.com>
2025-02-04 00:17:38 +0600

    initial commit

---
+++ numgame.py
@@ -0,0 +1,3 @@
+number = float(input("Enter any number you want to be calculated into the function : "))
+changed_result = (((number * 2) + 10) / 2) - number
+print("The changed value of your number is : ", changed_result)
